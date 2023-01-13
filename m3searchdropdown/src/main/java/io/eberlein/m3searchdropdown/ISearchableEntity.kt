821179e3f85b40e6eb2e987de34f19cfbb71172c
Pascal Eberlein <pascal@eberlein.io>
2023-01-13 18:36:20 +0100

    removed interfaces

--- m3searchdropdown/src/main/java/io/eberlein/m3searchdropdown/ISearchableEntity.kt
+++
@@ -1,5 +0,0 @@
-package io.eberlein.m3searchdropdown
-
-interface ISearchableEntity {
-    fun getValue(): String
-}
--- m3searchdropdown/src/main/java/io/eberlein/m3searchdropdown/ISearchableViewModel.kt
+++
@@ -1,5 +0,0 @@
-package io.eberlein.m3searchdropdown
-
-interface ISearchableViewModel <EntityType: ISearchableEntity> {
-    fun search(name: String, cb: (List<EntityType>) -> Unit)
-}
